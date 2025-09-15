6b4c2c28d4ef0e4ca85f5c5257e6b558bcb236d3
Yongju Lee <dydwn2001@gmail.com>
2025-09-14 21:06:54 -0400

    Routing does not work somehow

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './Home.jsx'
+import About from './About.jsx'
+import { Routes, Route } from "react-router-dom"
+import MainRouter from './MainRouter.jsx'
+function App() {
+  
+
+  return (<>
+    <MainRouter/>
+</>
+    
+      
+  )
+}
+
+export default App
---
+++ src/Education.jsx
@@ -0,0 +1,8 @@
+
+export default function Education() {
+  return (
+    <div>
+      <p>Education</p>
+    </div>
+  )
+}
